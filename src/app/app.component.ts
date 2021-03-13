45191622852203b9c3a35c6440770368f2107377
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-03-13 08:29:41 -0300

    Cadastro security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Observable } from "rxjs";
+import { Component } from "@angular/core";
+import { AuthService } from "./services/auth.service.service";
+import { User } from "./model/user";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+  constructor(private authService: AuthService) {}
 
-
-
-
-
-
-
-
-
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+    this.isLoggedOut$ = this.authService.isLoggedOut$;
+  }
 }
---
+++ src/app/services/auth.service.service.ts
@@ -0,0 +1,29 @@
+import { BehaviorSubject, Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "../model/user";
+
+export const ANONYNOUS_USER: User = {
+  id: undefined,
+  email: "",
+};
+
+@Injectable()
+export class AuthService {
+  private subject = new BehaviorSubject<User>(ANONYNOUS_USER);
+
+  user$: Observable<User> = this.subject.asObservable();
+
+  isLoggedIn$: Observable<boolean> = this.user$.map((user) => !!user.id);
+
+  isLoggedOut$: Observable<boolean> = this.isLoggedIn$.map((login) => !login);
+
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http
+      .post<User>("/api/signup", { email, password })
+      .shareReplay()
+      .do((user) => this.subject.next(user));
+  }
+}
