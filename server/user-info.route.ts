b3d2b698e4af5744befaa90a64055621b261e200
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-03-19 08:48:06 -0300

    AUTHO

---
+++ server/user-info.route.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { db } from "./database";
+export function userInfo(req: any, res: Response) {
+  const userInfo = req.user;
+  console.log("Checking if user exists", userInfo);
+  let user = db.findUserByEmail(userInfo.email);
+  if (!user) {
+    db.createUser(userInfo.email, userInfo.sub);
+  }
+  res.status(200).json({email: user.email})
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,66 +1,98 @@
-
-import {filter} from 'rxjs/operators';
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable, BehaviorSubject} from "rxjs";
-import {User} from "../model/user";
-import * as auth0 from 'auth0-js';
-import {Router} from "@angular/router";
+import { filter } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
+import { User } from "../model/user";
+import * as auth0 from "auth0-js";
+import { Router } from "@angular/router";
+import * as moment from "moment";
 
 export const ANONYMOUS_USER: User = {
-    id: undefined,
-    email: ''
+  id: undefined,
+  email: "",
 };
 
 const AUTH_CONFIG = {
-    clientID: 'hHhF4PWGY7vxLQH2HatJaUOertB1dDrU',
-    domain: "angularsecuritycourse.auth0.com"
+  clientID: "hash",
+  domain: "angularsecuritycourse.auth0.com",
 };
-
 
 @Injectable()
 export class AuthService {
+  auth0 = new auth0.WebAuth({
+    clientID: AUTH_CONFIG.clientID,
+    domain: AUTH_CONFIG.domain,
+    responseType: "token id_token",
+    redirectUri: "https://localhost:4200/lessons",
+    scope: "openid email",
+  });
 
-    auth0 = new auth0.WebAuth({
-        clientID: AUTH_CONFIG.clientID,
-        domain: AUTH_CONFIG.domain,
-        responseType: 'token id_token',
-        redirectUri: 'https://localhost:4200/lessons'
+  private userSubject = new BehaviorSubject<User>(undefined);
+
+  user$: Observable<User> = this.userSubject
+    .asObservable()
+    .pipe(filter((user) => !!user));
+
+  constructor(private http: HttpClient, private router: Router) {
+    if (this.isLoggedIn()) {
+      this.userInfo();
+    }
+  }
+
+  login() {
+    this.auth0.authorize({ initialScreen: "login" });
+  }
+
+  signUp() {
+    this.auth0.authorize({ initialScreen: "signUp" });
+  }
+
+  retriveAuthInfoFromUrl() {
+    this.auth0.parseHash((err, authResult) => {
+      if (err) {
+        console.log("Could not parse the hash", err);
+        return;
+      } else if (authResult && authResult.idToken) {
+        window.location.hash = "";
+        console.log("Authentication successful, authResult", authResult);
+
+        this.setSession(authResult);
+        this.userInfo();
+      }
     });
+  }
 
-    private userSubject = new BehaviorSubject<User>(undefined);
+  userInfo() {
+    this.http
+      .put<User>("/api/userinfo", null)
+      .shareReplay()
+      .do((user) => this.userSubject.next(user))
+      .subscribe();
+  }
 
-    user$: Observable<User> = this.userSubject.asObservable().pipe(filter(user => !!user));
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    this.router.navigate(["/lessons"]);
+  }
 
-    constructor(private http: HttpClient, private router: Router) {
+  isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
 
-    }
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
 
-    login() {
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
 
-    }
-
-    signUp() {
-
-    }
-
-    logout() {
-
-    }
-
-    public isLoggedIn() {
-        return false;
-    }
-
-    isLoggedOut() {
-        return !this.isLoggedIn();
-    }
-
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+  }
 }
-
-
-
-
-
-
-
